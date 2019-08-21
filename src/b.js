013af210f64d5d8a827fa8d33560855dbcd76835
midfer <13726249821@163.com>
2019-08-21 14:56:12 +0800

    test gcm for windows 2

--- src/b.js
+++ src/b.js
@@ -5,4 +5,5 @@ function printNoHello() {
 function printNoHi() {
     console.log('printNoHi');
     console.log('Git-Credential-Manager-for-Windows');
+    console.log('Git-Credential-Manager-for-Windows2');
 }
